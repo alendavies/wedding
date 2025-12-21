aa7f56d9279ae426a4460dca4f0331918e741225
alendavies <alendavies10@gmail.com>
2025-12-21 15:30:52 -0300

    fix: adjust spacing and font sizes in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,14 +9,14 @@ const Footer: React.FC = () => (
             pt-24
             pb-8
             text-center
-            space-y-6
+            space-y-8
         '
     >
         {/* FRASE */}
         <p
             className='
                 font-script
-                text-[36px]
+                text-[32px]
                 md:text-[44px]
                 leading-none
                 text-[#6f6f63]
@@ -35,6 +35,20 @@ const Footer: React.FC = () => (
             '
         >
             Azul & Pablo
+        </p>
+
+        {/* DIVISOR SUTIL */}
+        <div className='w-8 h-px bg-[#8f9d87]/40 mx-auto' />
+
+        {/* CRÉDITO */}
+        <p
+            className='
+                text-[11px]
+                tracking-wide
+                text-[#9a9a8f]
+            '
+        >
+            Desarrollado por Alen Davies · 2026
         </p>
     </section>
 );
