613df5190c5612c9c4cbabffb90dc2ad4ef1eca8
alendavies <alendavies10@gmail.com>
2026-02-02 15:26:56 -0300

    fix: add image for location section

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -3,7 +3,11 @@ import Button from './Button';
 
 const Location: React.FC = () => (
     <section className='w-full max-w-2xl px-8 py-20 bg-[rgb(var(--color-surface)/1)] text-center space-y-10'>
-        <div className='text-[28px] opacity-70'>🥂</div>
+        <img
+            src='/copas.png'
+            alt='Ubicación'
+            className='w-16 h-16 md:w-20 md:h-20 mx-auto'
+        />
 
         <h3 className='font-serif uppercase tracking-[0.45em] text-[14px] md:text-[16px] text-[#2f2f2a] font-semibold'>
             Celebración
